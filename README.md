c76fb188fa39d20050f99eb3ccf8d5ff923d56bc
Akindele Akinfenwa <Dele@Akindeles-MacBook-Pro.local>
2022-01-17 18:24:42 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# CreatingPythonAPI
+# PythonAPI
+
+This project is to showcase building an API using Python & Flask
+
+## Introduction
+
+---
+
+This project was built from the ground-up to showcase building an API using Python and Flask.
+
+## Authorization
+
+---
+
+This API requires no authorization.
+
+## Responses
+
+---
+
+The GET response returns a list of spells in JSON format
+
+![GET-request](./util/GET-request.png)
+
+In order to add or edit a spell the data fields must be filled with the appropriate data type.
+
+![Schema](./util/Schema.png)
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import request
 from flask import jsonify
 from peewee import *
 from playhouse.shortcuts import model_to_dict
+from playhouse.shortcuts import dict_to_model
 from spellList import spell_list
 
 db = PostgresqlDatabase('spells', user='postgres',
@@ -34,7 +35,7 @@ i = 0
 while i < (len(spell_list)):
     for key in spell_list[i]:
         cast = Spells(name=spell_list[i]["name"], desc=spell_list[i]["desc"],
-                      spell_range=spell_list[i]["range"], level=spell_list[i]["level"], school=spell_list[i]["school"])
+                      spell_range=spell_list[i]["spell_range"], level=spell_list[i]["level"], school=spell_list[i]["school"])
         cast.save()
         i += 1
 
@@ -42,20 +43,50 @@ while i < (len(spell_list)):
 app = Flask(__name__)
 
 
-@app.route('/spells', methods=['GET'])
-@app.route('/spells/<id>', methods=['GET'])
+@app.route('/spells', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/spells/<id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def spell(id=None):
-    if id:
+    # GET by ID
+    if request.method == 'GET':
+        if id:
+            spell = Spells.get(Spells.id == id)
+            cast = model_to_dict(spell)
+            return jsonify(cast)
+        # GET all
+        else:
+            spell = []
+            # Iterate over a query that gets every Spell
+            for casts in Spells.select():
+                # Convert each spell from Python object to Dictionary
+                spell.append(model_to_dict(casts))
+            # Convert list of dictionaries into JSON and return to server
+            return jsonify(spell)
+
+        # POST request
+    if request.method == 'POST':
+        spell = request.get_json()
+        spell = dict_to_model(Spells, spell)
+        spell.save()
+        spell = model_to_dict(spell)
+        return jsonify(spell)
+
+        # DELETE request
+    if request.method == 'DELETE':
         spell = Spells.get(Spells.id == id)
-        cast = model_to_dict(spell)
-        return jsonify(cast)
-    else:
-        spell = []
-        # Iterate over a query that gets every Spell
-        for casts in Spells.select():
-            # Convert each spell from Python object to Dictionary
-            spell.append(model_to_dict(casts))
-        # Convert list of dictionaries into JSON and return to server
+        spell.delete_instance()
+        return jsonify({'Deleted': True})
+
+        # PUT Request
+    if request.method == 'PUT':
+        updated_spell = request.get_json()
+        spell = Spells.get(Spells.id == id)
+        spell.name = updated_spell['name']
+        spell.desc = updated_spell['desc']
+        spell.spell_range = updated_spell['spell_range']
+        spell.level = updated_spell['level']
+        spell.school = updated_spell['school']
+        spell.save()
+        spell = model_to_dict(spell)
         return jsonify(spell)
 
 
